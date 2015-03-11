30b24d07fc77e63b90d1f022316d3df4e924c1fd
PeterTheOne <petertheone@gmail.com>
2015-03-12 00:35:48 +0100

    init

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'config.php';
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+
+$pdo = new PDO('mysql:host=localhost;dbname=' . PDO_DATABASE . ';charset=utf8', PDO_USERNAME, PDO_PASSWORD);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+$statuses = $connection->get('search/tweets', array(
+    'q' => 'GPG Fingerprint',
+    'result_type' => 'recent',
+    'count' => '20',
+    'include_entities' => 'false'
+));
+
+$insertStatement = $pdo->prepare('
+        INSERT IGNORE INTO fingerprint
+        (
+            statusid,
+            createdat,
+            text,
+            userid,
+            screenname,
+            fingerprint,
+            fetched
+        )
+        VALUES (
+            :statusid,
+            :createdat,
+            :text,
+            :userid,
+            :screenname,
+            :fingerprint,
+            NOW()
+        )
+    ');
+
+function matchFingerprint($text) {
+    $result = preg_match("@([A-Z0-9]{4} ){7}[A-Z0-9]{4}@", $text, $matches);
+    if ($result === 1) {
+        return $matches[0];
+    }
+    return '';
+}
+
+foreach ($statuses->statuses as $status) {
+    $fingerprint = matchFingerprint($status->text);
+    $createdat = date("Y-m-d H:i:s", strtotime($status->created_at));
+    $insertStatement->bindParam(':statusid', $status->id_str);
+    $insertStatement->bindParam(':createdat', $createdat);
+    $insertStatement->bindParam(':text', $status->text);
+    $insertStatement->bindParam(':userid', $status->user->id_str);
+    $insertStatement->bindParam(':screenname', $status->user->screen_name);
+    $insertStatement->bindParam(':fingerprint', $fingerprint);
+    $insertStatement->execute();
+}
+
+echo '<h2>found fingerprints</h2>';
+
+$statement = $pdo->query('SELECT * FROM fingerprint WHERE fingerprint <> \'\'');
+$result = $statement->fetchAll();
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';
+
+
+
+echo '<pre>';
+print_r($statuses);
+echo '</pre>';
+
---
+++ sample-config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+
+define('PDO_DATABASE', '');
+define('PDO_USERNAME', '');
+define('PDO_PASSWORD', '');
