b4e19a8c19ba4335ea449314fd09f69ba1eda2cb
PeterTheOne <petertheone@gmail.com>
2015-03-28 21:03:54 +0100

    make a little more pretty..

--- index.php
+++ index.php
@@ -10,16 +10,51 @@ $pdo = new PDO('mysql:host=localhost;dbname=' . PDO_DATABASE . ';charset=utf8', 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
-echo '<h2>found fingerprints</h2>';
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
 
+        <title>fingerprint-tweet</title>
+
+    </head>
+    <body>
+
+    <h1>fingerprint-tweet</h1>
+    <p>
+        Tweets with <a href="https://en.wikipedia.org/wiki/Public_key_fingerprint">PGP fingerprint</a> found since 9. March 2015.<br />
+        Created by <a href="https://twitter.com/petertheone">PeterTheOne</a>,
+        source on <a href="https://github.com/PeterTheOne/fingerprint-tweet">GitHub</a>.<br />
+        Don't trust me, don't trust twitter and don't trust the MIT. ^^°
+    </p>
+
+    <table>
+        <tr>
+            <th>created</th>
+            <th>screenname</th>
+            <th>fingerprint</th>
+            <th>signature</th>
+            <th>key</th>
+        </tr>
+<?php
 $statement = $pdo->query('SELECT * FROM fingerprint WHERE fingerprint <> \'\' ORDER BY createdat DESC');
 $result = $statement->fetchAll();
 
-echo '<ul>';
 foreach($result as $line) {
-    echo '<li>';
-    echo '<a href="https://twitter.com/' . $line->screenname . '">' . $line->screenname . '</a>: ';
-    echo '<a href="https://twitter.com/' . $line->screenname . '/status/' . $line->statusid . '">' . $line->fingerprint . '</a>';
-    echo '</li>';
+    $createdat = date("d.m.Y H:i:s", strtotime($line->createdat));
+    $fetched = date("d.m.Y H:i:s", strtotime($line->fetched));
+    $keyId = substr(str_replace(' ', '', $line->fingerprint), -16);
+    echo '<tr>';
+    echo '<td title="fetched: ' . $fetched . '">' . $createdat . '</td>';
+    echo '<td><a href="https://twitter.com/' . $line->screenname . '">@' . $line->screenname . '</a></td>';
+    echo '<td><a href="https://twitter.com/' . $line->screenname . '/status/' . $line->statusid . '">' . $line->fingerprint . '</a></td>';
+    echo '<td><a href="https://pgp.mit.edu/pks/lookup?op=vindex&search=0x' . $keyId . '">' . $keyId . '</a></td>';
+    echo '<td><a href="https://pgp.mit.edu/pks/lookup?op=get&search=0x' . $keyId . '">key</a></td>';
+    echo '</tr>';
 }
-echo '</ul>';
+?>
+    </table>
+
+    </body>
+</html>
